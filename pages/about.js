d335891d46373a814d3f01cf2717b02b3dac24c9
Paolo U <paolourciullo@gmail.com>
2021-07-18 12:33:59 -0400

    add posts

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout'
+
+export default function AboutPage() {
+  return (
+    <Layout title="About Devspace">
+      <h1 className="text-5xl border-b-4 pb-5 font-bold">About</h1>
+
+      <div className="bg-white shadow-md rounded-lg px-10 py-6 mt-6">
+        <h3 className="text-2xl mb-5">Devspace blog</h3>
+        <p className="mb-3">This is a blog built with Next.js and markdown</p>
+        <p>
+          <span className="font-bold">Version 1.0.0</span>
+        </p>
+      </div>
+    </Layout>
+  )
+}
