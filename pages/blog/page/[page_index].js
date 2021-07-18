8683bac9002455d59e77ce2ccb43887967b39af7
Paolo U <paolourciullo@gmail.com>
2021-07-18 15:46:52 -0400

    add pagination buttons

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -4,10 +4,11 @@ import Link from 'next/link'
 import matter from 'gray-matter'
 import Layout from '../../../components/Layout'
 import Post from '../../../components/Post'
+import Pagination from '../../../components/Pagination'
 import { sortByDate } from '../../../utils'
 import { POSTS_PER_PAGE } from '../../../config'
 
-export default function BlogPage({ posts }) {
+export default function BlogPage({ posts, numPages, currentPage }) {
   return (
     <Layout>
       <h1 className="text-5xl border-b-4 p-5 font-bold">Blog 🕺</h1>
@@ -17,6 +18,8 @@ export default function BlogPage({ posts }) {
           <Post key={index} post={post} />
         ))}
       </div>
+
+      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
@@ -33,14 +36,16 @@ export async function getStaticPaths() {
       params: { page_index: i.toString() },
     })
   }
-  console.log(paths)
+  // console.log(paths)
   return {
     paths,
     fallback: false,
   }
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  const page = parseInt((params && params.page_index) || 1)
+
   const files = fs.readdirSync(path.join('posts'))
 
   const posts = files.map((filename) => {
@@ -58,9 +63,17 @@ export async function getStaticProps() {
     }
   })
 
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  const pageIndex = page - 1
+  const orderedPosts = posts
+    .sort(sortByDate)
+    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      posts: orderedPosts,
+      numPages,
+      currentPage: page,
     },
   }
 }
