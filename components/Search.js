eff0ae4cf1233328b539266b80617355792734c1
Paolo U <paolourciullo@gmail.com>
2021-07-18 20:11:40 -0400

    add searchresults component

--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,24 @@
 import { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
+import SearchResults from './SearchResults'
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState('')
   const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    const getResults = async () => {
+      if (searchTerm === '') {
+        setSearchResults([])
+      } else {
+        const res = await fetch(`/api/search?q=${searchTerm}`)
+        const { results } = await res.json()
+        setSearchResults(results)
+      }
+    }
+
+    getResults()
+  }, [searchTerm])
 
   return (
     <div className="relative bg-gray-600 p-4">
@@ -24,6 +39,7 @@ export default function Search() {
           </form>
         </div>
       </div>
+      <SearchResults results={searchResults} />
     </div>
   )
 }
